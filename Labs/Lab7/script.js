115dd08e69a5ace26796e849c03575db083b04f6
AndoraKoru <230125130+AndoraKoru@users.noreply.github.com>
2025-09-29 19:54:13 +0300

    Lab7
    First Javascript exercise

---
+++ Labs/Lab7/script.js
@@ -0,0 +1,28 @@
+let firstName = "Alexandra";
+let newSentence = "Hello my name is " + firstName;
+
+// console.log(newSentence);
+// console.log("Hello, I am " + firstName + ", I am called " + firstName + ", call me " + firstName + "... because my mother named me " + firstName + ".");
+console.log(`Hello, I am ${firstName}, I am called ${firstName}, call me ${firstName}, because my mother named me ${firstName} .... ${firstName}`);
+
+// alert("Hi how are you")
+let num1;
+const num2 = 5;
+
+num1 = 10;
+
+let result = num1 + num2;
+console.log(result);
+
+let mixedResult = firstName + num1;
+console.log(mixedResult);
+
+let a = 10;
+let b = 4;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
